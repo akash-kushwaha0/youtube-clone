35d4eacf869d9c474e363a2c41fc2acb7d613c3b
akash kushwaha <akashkushwaha76986@gmail.com>
2023-04-11 11:00:36 +0530

    API fetched,Added  VideoCard, ChannelCard

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,14 +1,27 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 
+import { fetchFromAPi } from "../utils/fetchFromAPI";
 import { Sidebar, Videos } from './';
 
 const Feed = () => {
+
+  const [selectedCategory, setselectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() =>{
+    fetchFromAPi(`search?part=snippet&q=${selectedCategory}`)
+    .then((data) => setVideos(data.items))
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       {/* Box = color theme */}
       <Box sx={{ height: { sx: 'auto', md: "92vh" }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
-        <Sidebar />
+        <Sidebar 
+          selectedCategory={selectedCategory}
+          setselectedCategory={setselectedCategory}
+        />
 
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff' }}>
           Copyright 2023 METube
@@ -17,9 +30,9 @@ const Feed = () => {
 
       <Box p={2} sx={{ overflow: 'auto', height: '98vh', flex: 2 }}>
         <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: 'white' }}>
-          New <span style={{ color: '#F31503' }}>Videos</span>
+          {selectedCategory} <span style={{ color: '#F31503' }}>Videos</span>
         </Typography>
-        <Videos videos={[]}  /> 
+        <Videos videos={videos} />
       </Box>
     </Stack>
   )
